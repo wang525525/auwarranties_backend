2e3da042d7ba5f749ab570f298c02d0c757e9fe1
wang525525 <wang525525@gmail.com>
2020-08-10 11:11:39 +0800

    Pet related part is removed.

--- src/api/resolvers/UserResolver.ts
+++ src/api/resolvers/UserResolver.ts
@@ -1,8 +1,6 @@
-import { FieldResolver, Query, Resolver, Root } from 'type-graphql';
+import { Query, Resolver } from 'type-graphql';
 import { Service } from 'typedi';
 
-import { User as UserModel } from '../models/User';
-import { PetService } from '../services/PetService';
 import { UserService } from '../services/UserService';
 import { User } from '../types/User';
 
@@ -11,18 +9,12 @@ import { User } from '../types/User';
 export class UserResolver {
 
     constructor(
-        private userService: UserService,
-        private petService: PetService
+            private userService: UserService
         ) {}
 
     @Query(returns => [User])
     public users(): Promise<any> {
       return this.userService.find();
-    }
-
-    @FieldResolver()
-    public async pets(@Root() user: UserModel): Promise<any> {
-        return this.petService.findByUser(user);
     }
 
 }

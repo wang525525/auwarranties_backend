e7317173a90cce7ea59a8949729a1e6c3a88a0a1
wang525525 <wang525525@gmail.com>
2020-08-14 21:32:39 +0800

    unnecessary file is removed in policy items

--- src/api/repositories/PolicyItemRepository.ts
+++ src/api/repositories/PolicyItemRepository.ts
@@ -5,13 +5,4 @@ import { PolicyItem } from '../models/PolicyItem';
 @EntityRepository(PolicyItem)
 export class PolicyItemRepository extends Repository<PolicyItem>  {
 
-    /**
-     * Find All Pricing
-     */
-    public findByCoverId(coverid: number): Promise<any> {
-        return this.createQueryBuilder('policyitems')
-                            .where(`coverid=${coverid}`)
-                            .select(['*'])
-                            .getMany();
-    }
 }

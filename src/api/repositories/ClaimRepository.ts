58406c4197b8e3b2dbe7873d30b641ec98170742
wang525525 <wang525525@gmail.com>
2020-08-25 18:56:34 +0800

    return value is defined for claims

--- src/api/repositories/ClaimRepository.ts
+++ src/api/repositories/ClaimRepository.ts
@@ -12,6 +12,12 @@ export class ClaimRepository extends Repository<Claim>  {
                             .leftJoinAndSelect('claims.policy', 'policy')
                             .leftJoinAndSelect('policy.branchuser', 'users')
                             .leftJoinAndSelect('claims.claimstate', 'state')
+                            .select(['claims.claimid', 'claims.state', 'claims.adminresponded', 'claims.claimnumber', 'claims.dateclaim', 
+                                     'claims.claimdateseconds', 'claims.failedpart', 'claims.failurecause', 'claims.repairsrequired', 
+                                     'claims.claimtotal', 'claims.represponded', 
+                                     'policy.policyid', 'policy.policynumber', 
+                                     'users.companyname', 'users.userid', 
+                                     'state.stateid', 'state.statename'])
                             .take(limit)
                             .getMany();
     }
@@ -25,6 +31,12 @@ export class ClaimRepository extends Repository<Claim>  {
                             .leftJoinAndSelect('policy.branchuser', 'users')
                             .leftJoinAndSelect('claims.claimstate', 'state')
                             .where(`policy.branchid=${branchid}`)
+                            .select(['claims.claimid', 'claims.state', 'claims.adminresponded', 'claims.claimnumber', 'claims.dateclaim', 
+                                     'claims.claimdateseconds', 'claims.failedpart', 'claims.failurecause', 'claims.repairsrequired', 
+                                     'claims.claimtotal', 'claims.represponded', 
+                                     'policy.policyid', 'policy.policynumber', 
+                                     'users.companyname', 'users.userid', 
+                                     'state.stateid', 'state.statename'])
                             .getMany();
     }
 
